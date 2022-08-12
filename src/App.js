43cf83cc6018ef899a0de079fb3a66e7339b7ffb
thisiskushal31 <guptakushal131@gmail.com>
2022-08-12 13:32:08 +0530

    Button Fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Clients from "./components/Clients";
-import Footer from "./components/Footer";
-import Feature from "./components/Feature";
-import Home from "./components/Home";
+// import React, { useState, useEffect } from "react";
+// import Clients from "./components/Clients";
+// import Footer from "./components/Footer";
+// import Feature from "./components/Feature";
+// import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import ScrollToTop from "./components/ScrollToTop";
-import scrollreveal from "scrollreveal";
+// import ScrollToTop from "./components/ScrollToTop";
+// import scrollreveal from "scrollreveal";
 import "./sass/index.scss";
 // import Feature from "./components/Feature";
 function App() {
