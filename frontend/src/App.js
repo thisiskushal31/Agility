fb3b7fa902bcf636452623db21191c2d9f1400ff
thisiskushal31 <guptakushal131@gmail.com>
2022-09-11 22:28:46 +0530

    Folders Rearranged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./scss/main.scss";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
